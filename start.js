45f954ac609af261d0c9f332cf42a49005700c6e
Rosano Coutinho <github.com@xeari.com>
2026-02-07 15:24:28 +0100

    use svelte in express

--- start.js
+++ start.js
@@ -1,13 +1,17 @@
 import express from 'express';
+
 import main from './rs-bind/main.js';
 import adapter from './adapter.js';
 
-const port = process.env.PORT || 3000;
+import { handler } from './build/handler.js';
+
+const port = 3000;
 express()
-  .use(express.json())
-  .use(express.raw({
-    limit: '1mb',
-    type: '*/*',
-  }))
+  // .use(express.json())
+  // .use(express.raw({
+  //   limit: '1mb',
+  //   type: '*/*',
+  // }))
   .use(main.handler(adapter))
+  .use(handler)
   .listen(port, () => console.info(`> Running on port ` + port));
