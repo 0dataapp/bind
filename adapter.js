a8e5f0639557fc232b9ffcc73b4f4b8c0ffa5a47
Rosano Coutinho <github.com@xeari.com>
2026-02-08 15:53:01 +0100

    move storage folder

--- adapter.js
+++ adapter.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const _storage = path.join(__dirname, 'data');
+const _storage = path.join(__dirname, '__storage');
 
 import { simpleGit, CleanOptions } from 'simple-git';
 const git = simpleGit(_storage, {
