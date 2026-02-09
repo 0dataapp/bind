12d21ba8e229eb40648c6edd83f23f6764adc58b
Rosano Coutinho <github.com@xeari.com>
2026-02-09 10:44:45 +0100

    scaffold cloudron package

---
+++ cloudron/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ensure that this file executable in the app repo: chmod +x start.sh.
+
+mkdir -p /app/data/__storage
+
+chown -R cloudron:cloudron /app/data
+
+exec /usr/local/bin/gosu cloudron:cloudron npm run production
