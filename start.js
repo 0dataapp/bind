4111443bf87bff7bd21cdb81f3f8d113765fc559
Rosano Coutinho <github.com@xeari.com>
2026-02-11 16:58:29 +0100

    allow setting port by env

--- start.js
+++ start.js
@@ -8,7 +8,7 @@ import { tokens } from './tokens.js';
 import { handler } from './build/handler.js';
 
 const prefix = 'storage';
-const port = 3000;
+const port = process.env.PORT || 3000;
 express()
   .use(bind.options())
   .use(bind.webfinger({
