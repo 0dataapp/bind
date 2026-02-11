17630760930893e77a05f70a4f9f341b3d1dc859
Rosano Coutinho <github.com@xeari.com>
2026-02-11 14:29:27 +0100

    rename __storage

--- adapter.js
+++ adapter.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const _storage = path.join(process.env.DATA_DIRECTORY || process.cwd(), '__storage');
+const _storage = path.join(process.env.DATA_DIRECTORY || process.cwd(), '__main');
 
 import { simpleGit, CleanOptions } from 'simple-git';
 
