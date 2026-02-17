51839f9e2b9e36dd5502d7deb16fc5f1aaab5b71
Rosano Coutinho <github.com@xeari.com>
2026-02-17 14:17:40 +0100

    configure hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,7 @@
+import { auth } from '$lib/better-auth/config.js';
+import { svelteKitHandler } from 'better-auth/svelte-kit';
+import { building } from '$app/environment';
+
 import bind from 'bind-middleware';
 
 import storage from '$lib/adapter.js';
@@ -70,6 +74,7 @@ const main = async ({ event, resolve }) => {
 
 const prefix = 'storage';
 export const handle = sequence(
+  ({ event, resolve }) => svelteKitHandler({ event, resolve, auth, building }),
   _handle(bind.cors()),
 	_handle(bind.storage({
 	  getScope: tokens.getScope,
