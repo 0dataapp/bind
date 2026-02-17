858df590479398190d51a603761cb0f4886a771e
Rosano Coutinho <github.com@xeari.com>
2026-02-17 10:14:28 +0100

    spec db

---
+++ src/lib/db/main-tests.js
@@ -0,0 +1,119 @@
+import { describe, it, expect, throws, afterAll } from 'vitest';
+import fs from 'fs';
+import path from 'path';
+import mod from './main.js';
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const folder = '__tests';
+
+describe('collection', () => {
+
+	afterAll(() => {
+		const _folder = path.join(__dirname, folder);
+	  fs.readdirSync(_folder).forEach(e => fs.unlinkSync(path.join(_folder, e)));
+	})
+
+	const _collection = e => mod.collection(e || Math.random().toString(), {
+		folder,
+	});
+
+	it('throws if not string', () => {
+		expect(() => mod.collection()).toThrowError('missing collection name');
+	});
+	
+	it('returns object', () => {
+		expect(_collection()).toBeTypeOf('object');
+	});
+
+	describe('create', () => {
+
+		it('returns input', () => {
+			const item = {
+				id: Math.random().toString(),
+			};
+			expect(_collection().create(item)).toBe(item);
+		});
+
+		it('updates file', () => {
+			const collection = Math.random().toString();
+			const item = {
+				id: Math.random().toString(),
+			};
+			_collection(collection).create(item);
+			expect(JSON.parse(fs.readFileSync(path.join(__dirname, folder, `${ collection }.json`), 'utf8'))).toEqual({ items: [item] });
+		});
+
+	});
+
+	describe('getItems', () => {
+
+		it('returns items', () => {
+			const db = _collection();
+
+			const item = {
+				id: Math.random().toString(),
+			};
+			db.create(item);
+			
+			expect(db.getItems()).toEqual([item]);
+		});
+
+	});
+
+	describe('update', () => {
+
+		it('returns input', () => {
+			const db = _collection();
+
+			const id = Math.random().toString();
+			db.create({ id });
+
+			const key = Math.random().toString();
+			const item = { id, key };
+			expect(db.update(id, item)).toBe(item);
+		});
+
+		it('updates file', () => {
+			const collection = Math.random().toString();
+			const db = _collection(collection);
+
+			const id = Math.random().toString();
+			db.create({ id });
+
+			const key = Math.random().toString();
+			const item = { id, key };
+			db.update(id, item);
+
+			expect(JSON.parse(fs.readFileSync(path.join(__dirname, folder, `${ collection }.json`), 'utf8'))).toEqual({ items: [item] });
+		});
+
+	});
+
+	describe('delete', () => {
+
+		it('returns undefined', () => {
+			const db = _collection();
+
+			const id = Math.random().toString();
+			db.create({ id });
+
+			expect(db.delete(id)).toBe(undefined);
+		});
+
+		it('updates file', () => {
+			const collection = Math.random().toString();
+			const db = _collection(collection);
+
+			const id = Math.random().toString();
+			db.create({ id });
+
+			db.delete(id);
+			
+			expect(JSON.parse(fs.readFileSync(path.join(__dirname, folder, `${ collection }.json`), 'utf8'))).toEqual({ items: [] });
+		});
+
+	});
+
+});
---
+++ src/lib/db/main.js
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { LowSync } from 'lowdb';
+import { JSONFileSync } from 'lowdb/node';
+
+const mod = {
+
+	collection (collection, params = {}) {
+		if (!collection)
+			throw new Error('missing collection name');
+
+		const folder = path.join(__dirname, params.folder || '__data');
+		fs.mkdirSync(folder, { recursive: true });
+
+		let db;
+		const _this = {};
+		return Object.assign(_this, {
+
+			db: () => {
+				if (db)
+					return db;
+
+				return (db = new LowSync(new JSONFileSync(path.join(folder, `${ collection }.json`)), { items: [] }));
+			},
+
+			create: obj => {
+				_this.db().update(({ items }) => items.push(obj));
+
+				return obj;
+			},
+
+			getItems: () => _this.db().data.items,
+
+			update: (id, obj) => {
+				_this.db().update(({ items }) => Object.assign(items.filter(e => e.id === id).shift(), obj));
+
+				return obj;
+			},
+
+			delete: id => {
+				_this.db().update(({ items }) => items.splice(items.findIndex(e => e.id === id)));
+			},
+
+		});
+	}
+
+};
+
+export default mod;
