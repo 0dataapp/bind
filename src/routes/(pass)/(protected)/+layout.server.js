84a4b6ce5afca3971ec6e7776a92bfd9b50e82eb
Rosano Coutinho <github.com@xeari.com>
2026-02-18 07:02:17 +0100

    replace user

--- src/routes/(pass)/(protected)/+layout.server.js
+++ src/routes/(pass)/(protected)/+layout.server.js
@@ -1,8 +1,13 @@
+import { auth } from '$lib/better-auth/config';
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').LayoutServerLoad} */
-export async function load({ locals, url }) {
-	return !locals.user ? redirect(307, `/login?target=${ encodeURIComponent(`${ url.pathname }${ url.search }`) }`) : {
-		user: locals.user,
+export async function load({ request, url }) {
+	const session = await auth.api.getSession({
+		headers: request.headers,
+	});
+
+	return !session ? redirect(307, `/login?target=${ encodeURIComponent(`${ url.pathname }${ url.search }`) }`) : {
+		session,
 	};
 }
