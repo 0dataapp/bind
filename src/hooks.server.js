07453862315d6ff95d7c1ef84016d27b2acb4826
Rosano Coutinho <github.com@xeari.com>
2026-02-18 10:30:52 +0100

    rename adapter

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,7 +4,7 @@ import { building } from '$app/environment';
 
 import bind from 'bind-middleware';
 
-import storage from '$lib/adapter.js';
+import storage from '$lib/storage.js';
 import { tokens } from '$lib/tokens.js';
 
 import { sequence } from '@sveltejs/kit/hooks';
