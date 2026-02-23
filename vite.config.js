90b3ebbf99cfc1782cf158fa934af5fc261b88ba
Rosano Coutinho <github.com@xeari.com>
2026-02-23 10:23:46 +0100

    load local host/port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
 	return {
 		plugins: [sveltekit()],
 		test: {
@@ -18,6 +19,11 @@ export default defineConfig(({ mode }) => {
 					},
 				},
 			],
+		},
+		server: {
+			host: env.HOST,
+			port: env.PORT,
+			allowedHosts: [ env.HOST ],
 		},
 	};
 });
