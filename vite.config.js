147b761212f8b566b4a5f509e2552ffb5fb8fbea
Rosano Coutinho <github.com@xeari.com>
2026-02-23 10:23:39 +0100

    use new vite boilerplate

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.js',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*-tests.{js,ts}'],
-					exclude: ['**/ui-tests.{js,ts}'],
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [sveltekit()],
+		test: {
+			hideSkippedTests: true,
+			expect: { requireAssertions: true },
+			projects: [
+				{
+					extends: './vite.config.js',
+					test: {
+						name: 'server',
+						environment: 'node',
+    				include: ['src/**/*-tests.{js,ts}'],
+    				exclude: ['**/ui-tests.{js,ts}'],
+					},
 				},
-			},
-		],
-	},
+			],
+		},
+	};
 });
