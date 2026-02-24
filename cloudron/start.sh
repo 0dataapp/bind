1a5630efafeabbe1239e0ccb838d063a541ae6ec
Rosano Coutinho <github.com@xeari.com>
2026-02-24 11:21:15 +0100

    purge mkdir from cloudron

--- cloudron/start.sh
+++ cloudron/start.sh
@@ -6,8 +6,6 @@ if [[ ! -f /app/data/.env ]]; then
   printf "BETTER_AUTH_SECRET=\"$( openssl rand -hex 32 )\"\n" > /app/data/.env
 fi
 
-mkdir -p /app/data/__local
-
 chown -R cloudron:cloudron /app/data
 
 exec /usr/local/bin/gosu cloudron:cloudron node --env-file=/app/data/.env build
