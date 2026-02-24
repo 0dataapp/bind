abccb3c985476cef1b8f2cf4170d7798add364f2
Rosano Coutinho <github.com@xeari.com>
2026-02-24 11:56:26 +0100

    lint

--- src/lib/util.js
+++ src/lib/util.js
@@ -7,9 +7,9 @@ const mod = {
 	hex: {
 		encode: e => Array.from(new TextEncoder().encode(e)).map(byte => byte.toString(16).padStart(2, '0')).join(''),
 		decode: hex => {
-			  const bytes = new Uint8Array(hex.match(/../g).map(h => parseInt(h, 16)));
-			  return new TextDecoder().decode(bytes);
-			}
+		  const bytes = new Uint8Array(hex.match(/../g).map(h => parseInt(h, 16)));
+		  return new TextDecoder().decode(bytes);
+		},
 	},
 
 };
