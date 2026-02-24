a74b238cc9c5da907b4a9e88ea5f06bf6b907d07
Rosano Coutinho <github.com@xeari.com>
2026-02-24 10:20:25 +0100

    extract storage/disk

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,7 +4,7 @@ import { building } from '$app/environment';
 
 import bind from 'bind-middleware';
 
-import storage from '$lib/storage/main.js';
+import storage from '$lib/storage/disk/main.js';
 import oauth from '$lib/oauth-implicit/main.js';
 
 import { sequence } from '@sveltejs/kit/hooks';
--- src/lib/better-auth/config.js
+++ src/lib/better-auth/config.js
@@ -8,7 +8,7 @@ import database from '$lib/database/main.js';
 import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import oauth from '$lib/oauth-implicit/main.js';
-import storage from '$lib/storage/main.js';
+import storage from '$lib/storage/disk/main.js';
 
 export const auth = betterAuth({
   secret: building ? 'BUILD_SECRET_ONLY' : env.BETTER_AUTH_SECRET,
