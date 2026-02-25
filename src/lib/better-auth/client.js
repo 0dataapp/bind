c44f56045c891ef53ef2c3097d6da887264cc4ad
Rosano Coutinho <github.com@xeari.com>
2026-02-25 10:30:12 +0100

    tidy

--- src/lib/better-auth/client.js
+++ src/lib/better-auth/client.js
@@ -2,7 +2,13 @@ import { createAuthClient } from 'better-auth/svelte';
 
 import { usernameClient } from 'better-auth/client/plugins';
 
-export const { signIn, signUp, changePassword, updateUser } = createAuthClient({
+export const {
+	signIn,
+	signUp,
+
+	changePassword,
+	updateUser,
+} = createAuthClient({
 	plugins: [
 		usernameClient(),
 	],
