e97adee658a28ab93c15537729fec90848a5f657
Rosano Coutinho <github.com@xeari.com>
2026-02-26 08:25:20 +0100

    link to docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
-<a class="login" href="/login">Sign in</a>
+<p>Git personal data store for web apps. <a class="docs" href="https://bind.0data.app" rel="noreferrer" target="_blank">Learn more</a>.</p>
+
+<a class="login" href="/login" role="button">Sign in</a>
--- src/routes/ui-tests.js
+++ src/routes/ui-tests.js
@@ -13,6 +13,18 @@ test.describe('home', () => {
     
   });
 
+  test.describe('docs', () => {
+
+    test('href', ({ page }) => expect(page.locator('a.docs')).toHaveAttribute('href', 'https://bind.0data.app'));
+    
+    test('rel', ({ page }) => expect(page.locator('a.docs')).toHaveAttribute('rel', 'noreferrer'));
+
+    test('target', ({ page }) => expect(page.locator('a.docs')).toHaveAttribute('target', '_blank'));
+
+    test('text', ({ page }) => expect(page.locator('a.docs')).toHaveText('Learn more'));
+    
+  });
+
   test.describe('login', () => {
 
     test('links to page', ({ page }) => expect(page.locator('a.login')).toHaveAttribute('href', '/login'));
