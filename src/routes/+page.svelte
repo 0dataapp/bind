c7040a52b28ba44e0715a90d05a9f3196873e258
Rosano Coutinho <github.com@xeari.com>
2026-03-02 11:46:14 +0100

    separate learn more

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<p>Git personal data store for web apps. <a class="docs" href="https://bind.0data.app" rel="noreferrer" target="_blank">Learn more</a>.</p>
+<p>Git personal data store for web apps.</p>
 
-<a class="login" href="/login" role="button">Sign in</a>
+<p>
+	<a class="login" href="/login" role="button">Sign in</a>
+</p>
+
+<p>
+	<a class="docs" href="https://bind.0data.app" rel="noreferrer" target="_blank">Learn more</a>
+</p>
