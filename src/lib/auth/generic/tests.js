184bb60313c02444a2fe56f72e145651726dce14
Rosano Coutinho <github.com@xeari.com>
2026-03-03 14:27:51 +0100

    fix typo

--- src/lib/auth/generic/tests.js
+++ src/lib/auth/generic/tests.js
@@ -8,7 +8,7 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const _DATA_DIRECTORY = path.join(__dirname, '../../../__testing');
+const _DATA_DIRECTORY = path.join(__dirname, '../../../../__testing');
 const folder = path.join(_DATA_DIRECTORY, database._subdirectory());
 
 const uItem = params => Object.assign({
