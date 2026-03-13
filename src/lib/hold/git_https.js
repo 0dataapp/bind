776119f0c576ee1333e68b86dc2aea946e19215d
Unknown <noreply@example.com>
2026-03-13 16:44:40 +0100

    spec list

--- src/lib/hold/git_https.js
+++ src/lib/hold/git_https.js
@@ -110,6 +110,36 @@ const mod = {
 			await mod.git(mod.util._clonePath(cloneURL)).commit();
 		},
 
+		async list ({ target: _path }) {
+			const target = this._localPath(_path);
+			const _this = this;
+
+			const tree = (await mod.git(mod.util._clonePath(cloneURL)).repo.raw('ls-tree', '--format', '%(objecttype) %(objectname) %(objectsize:padded)%x09%(path)', 'HEAD', mod.util._gitPath(_path))).trim();
+
+			if (!tree.length)
+				return {};
+
+			return Promise.all(tree.split('\n').map(e => {
+				const [type, hash, size, path] = e.split(/\s+/);
+				return {
+					name: type === 'tree' ? `${ path }/` : path,
+					type,
+					hash,
+					size: size === '-' ? null : parseInt(size),
+				};
+			}).map(async e => {
+				const basename = e.name.match(new RegExp(`^${ _path.slice(1) }(.*)`)).pop();
+				return [
+					basename,
+					e.type === 'tree'
+					? { ETag: e.hash }
+					: await _this.meta({
+						target: path.join(_path, basename),
+					}),
+				];
+			})).then(Object.fromEntries);
+		},
+
 		async meta ({ target: _path }) {
 			const target = this._localPath(_path);
 			const stat = fs.statSync(target);
--- src/lib/hold/git_https/tests.js
+++ src/lib/hold/git_https/tests.js
@@ -1,4 +1,4 @@
-import { describe, test, expect, beforeAll } from 'vitest';
+import { describe, test, expect, beforeAll, beforeEach } from 'vitest';
 import mod from '../git_https.js';
 import stub from '../../stub.js';
 import util from '../util.js';
@@ -9,18 +9,19 @@ const testFolder = './__testing/git_https/';
 const cloneURL = 'testing-repo';
 const repo = `${ testFolder }${ util.hash(cloneURL) }/`;
 const filesystem = mod.filesystem(cloneURL);
+function resetRepo () {
+	fs.rmSync(testFolder, { recursive: true, force: true });
+
+	fs.mkdirSync(repo, { recursive: true });
+
+	return mod.git(repo)._init();
+};
 
 mod.folder = testFolder;
 
 describe('filesystem', () => {
 
-	beforeAll(() => {
-		fs.rmSync(testFolder, { recursive: true, force: true });
-
-		fs.mkdirSync(repo, { recursive: true });
-
-		return mod.git(repo)._init();
-	});
+	beforeAll(resetRepo);
 
 	describe('put', () => {
 
@@ -151,6 +152,65 @@ describe('filesystem', () => {
 			expect(meta.ETag).not.toBe((await filesystem.meta({
 				target: parents[0] + '/',
 			})).ETag);
+		});
+
+	});
+
+	describe('list', () => {
+
+		beforeEach(resetRepo);
+
+		test('file', async () => {
+			const handle = stub.ulid();
+			const target = stub.basename();
+			const data = Math.random().toString();
+			const encoding = 'text/plain';
+			await filesystem.put({
+				handle,
+				target,
+				data,
+				ancestors: [],
+				meta: stub.headers(encoding),
+			});
+			await mod.git(repo).commit(true);
+			const _target = filesystem._localPath(target);
+			const stat = fs.statSync(_target);
+			expect(await filesystem.list({
+				handle,
+				target: '/',
+			})).toEqual({
+				[target]: await filesystem.meta({
+					handle,
+					target,
+				}),
+			});
+		});
+
+		test('folder', async () => {
+			const handle = stub.ulid();
+
+			const parent = stub.ulid();
+			const ancestors = [parent].map(e => filesystem._localPath(e) + '/');
+
+			const target = path.join(parent, stub.basename());
+			const data = Math.random().toString();
+			await filesystem.put({
+				handle,
+				target,
+				data,
+				ancestors,
+				meta: stub.headers('text/plain'),
+			});
+			await mod.git(repo).commit(true);
+			expect(await filesystem.list({
+				handle,
+				target: '/',
+			})).toEqual({
+				[parent + '/']: await filesystem.meta({
+					handle,
+					target: parent + '/',
+				}),
+			});
 		});
 
 	});
