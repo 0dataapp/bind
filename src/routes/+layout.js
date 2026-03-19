2de51779d00927254c636e0cf91a1fd118aec8b3
Rosano <git@xeari.com>
2026-03-19 15:04:07 +0100

    guard empty route

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -5,13 +5,13 @@ export function load({ route }) {
 	return {
 		...props,
 		navigation: [].concat(
-			route.id.match(/\(protected\)\/(?!dash)/)
+			route?.id.match(/\(protected\)\/(?!dash)/)
 			? { path: '/dash', title: 'Dashboard' }
 			: []
 		).concat(
-			route.id.match('(protected)')
+			route?.id.match('(protected)')
 			? [].concat(
-				route.id === '/(protected)/dash'
+				route?.id === '/(protected)/dash'
 					? { path: '/account', title: 'Account' }
 					: []
 					).concat({ path: '/logout', title: 'Sign out' })
