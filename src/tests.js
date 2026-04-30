6c0205a34b6611c526fb21ab11b1ee4aada63c40
Rosano <git@xeari.com>
2026-04-30 14:38:29 +0200

    simplify ignore test

--- src/tests.js
+++ src/tests.js
@@ -1,14 +1,12 @@
 import { describe, test, expect } from 'vitest';
 import fs from 'fs';
 
-describe('ignore', () => {
+test('sync .gitignore and .docerignore', () => {
 
-	test('sync', () => {
-		const _read = e => fs.readFileSync(`.${ e }ignore`, 'utf8').split('\n').filter(e => !!e && !e.startsWith('#'));
-		expect(_read('git')).toEqual(_read('docker').filter(e => ![
-			'.git',
-			'docs',
-		].includes(e)));
-	});
+	const _read = e => fs.readFileSync(`.${ e }ignore`, 'utf8').split('\n').filter(e => !!e && !e.startsWith('#'));
+	expect(_read('git')).toEqual(_read('docker').filter(e => ![
+		'.git',
+		'docs',
+	].includes(e)));
 
 });
